0376bfe6376268e2f0af96fecc8b4d37e33f609b
Tiahna Padilla <tiahna.padilla@gmail.com>
2025-03-16 13:25:54 -0500

    created file

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    // If deploying to a subdirectory, add basePath here:
+    // basePath: '/your-repository-name', // Replace with your repository name
+  };
+  
+  module.exports = nextConfig;
